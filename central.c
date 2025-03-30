17c40cf3b1542e35bd35c722355bd45842e13eae
William Saran <williamsaranjr@gmail.com>
2025-03-30 20:46:18 -0300

    Criar variáveis para os valores que estavam hard coded

--- central.c
+++ central.c
@@ -12,11 +12,25 @@
 
 #define NUMERO_VIATURAS 15
 
-pthread_t threads[NUMERO_VIATURAS];
-bool viaturas[NUMERO_VIATURAS] = {false}; // Array para verificar se a viatura está em uso
-int thread_ids[NUMERO_VIATURAS];
-int viaturasEmUso = 0;
+// Variáveis que comandam o tempo da ocorrência
+const int MINIMO_DESLOCAMENTO = 2;
+const int MAXIMO_DESLOCAMENTO = 5;
+const int MINIMO_ATENDIMENTO = 8;
+const int MAXIMO_ATENDIMENTO = 20;
 
+// Variáveis relacionadas às viaturas
+pthread_t viaturas[NUMERO_VIATURAS]; // Cada thread é uma viatura
+int idsViaturas[NUMERO_VIATURAS]; // Array de identificador único de viaturas
+
+// Variáveis relacionadas ao controle da central
+bool viaturasDisponiveis[NUMERO_VIATURAS] = {false}; // Array para verificar se a viatura está em uso
+int viaturasEmUso = 0;
+pthread_mutex_t viaturasMutex = PTHREAD_MUTEX_INITIALIZER; // Mutex para fazer alterações nas variáveis acima de forma sincronizada
+
+// Variáveis relacionadas à dificuldade
+int chance_de_crime = 60;
+
+// Gerar os identificadores únicos de cada viatura
 void inicializar_viaturas(int v[], int n) {
     for (int i = 0; i < n; i++) {
         v[i] = i + 1; // Inicializa os IDs das viaturas
@@ -27,37 +41,42 @@ void* atender_ocorrencia(void* arg) {
     time_t start = time(NULL); // Marca o início do atendimento
     int codigo = *(int*)arg; // Código identificador
 
-    printf("[VIATURA %d] Se deslocando ao local da ocorrência\n", codigo);
-    sleep(rand() % 5 + 5); // Simula o tempo de deslocamento
+    printf("[VIATURA %04d] Se deslocando ao local da ocorrência\n", codigo);
+    int deslocamento = rand() % MAXIMO_DESLOCAMENTO + MINIMO_DESLOCAMENTO;
+    sleep(deslocamento); // Simula o tempo de deslocamento
 
-    printf("[VIATURA %d] Chegou ao local da ocorrência\n", codigo);
-    sleep(rand() % 11 + 20); // Simula o tempo de atendimento
+    printf("[VIATURA %04d] Chegou ao local da ocorrência em %d minutos\n", codigo, deslocamento);
+    sleep(rand() % MAXIMO_ATENDIMENTO + MINIMO_DESLOCAMENTO); // Simula o tempo de atendimento
 
     time_t finish = time(NULL); // Fim do atendimento
     long tempo = (long) difftime(finish, start); // Tempo do atendimento em 'minutos'
-    printf("[VIATURA %d] Atendimento concluído em %lu minutos\n", codigo, tempo);
+    printf("[VIATURA %04d] Atendimento concluído em %lu minutos\n", codigo, tempo);
 
-    viaturas[codigo - 1] = false; // Libera a viatura
+    pthread_mutex_lock(&viaturasMutex);
+    viaturasDisponiveis[codigo - 1] = false; // Libera a viatura
     viaturasEmUso--; // Decrementa o contador de viaturas em uso
+    pthread_mutex_unlock(&viaturasMutex);
     pthread_exit(NULL); // Termina a thread
 }
 
 int main() {
+    // Inicializa um timer aleatório
     srand(time(NULL));
 
-    inicializar_viaturas(thread_ids, NUMERO_VIATURAS);
+    inicializar_viaturas(idsViaturas, NUMERO_VIATURAS);
 
     while (true) {
-        int n = rand() % 101; // Gera um número de 1 a 100
+        int n = rand() % 101; // Gera um número de 0 a 100
 
-        if (n < 100) { // Se for menor que 60, há uma ocorrência
-            n = rand() % NUMERO_VIATURAS; // Seleciona uma viatura aleatória
+        if (n < chance_de_crime) { // Se estiver abaixo da chance de crime, uma nova ocorrência é criada
             printf("[CENTRAL] Ocorrência detectada! Buscando viaturas...\n");
             for (int i = 0; i < NUMERO_VIATURAS; i++) {
-                if (!viaturas[i]) { // Se a viatura não estiver em uso
+                if (!viaturasDisponiveis[i]) { // Se a viatura não estiver em uso
+                    pthread_mutex_lock(&viaturasMutex);
                     viaturasEmUso++; // Incrementa o número de viaturas em uso
-                    viaturas[i] = true; // Marca a viatura como em uso
-                    pthread_create(&threads[i], NULL, atender_ocorrencia, &thread_ids[i]);
+                    viaturasDisponiveis[i] = true; // Marca a viatura como em uso
+                    pthread_mutex_unlock(&viaturasMutex);
+                    pthread_create(&viaturas[i], NULL, atender_ocorrencia, &idsViaturas[i]);
                     printf("[CENTRAL] Viatura %d designada para a ocorrência\n", i + 1);
                     break;
                 } else if (i == NUMERO_VIATURAS - 1) {
@@ -68,6 +87,6 @@ int main() {
             printf("[CENTRAL] Aguardando ocorrências...\n");
         }
         printf("[CENTRAL] Total de viaturas em uso: %d\n\n", viaturasEmUso);
-        sleep(3); // Espera 3 minuto para verificar novas ocorrências
+        sleep(3); // Espera 3 minutos para verificar novas ocorrências
     }
 }
