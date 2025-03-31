46ee01f5746e86d220bcdac355c1b707127f7c07
William Saran <134084734+williamsaranjr@users.noreply.github.com>
2025-03-31 00:30:35 +0000

    Criar as condições de sincronização entre as threads

--- central.c
+++ central.c
@@ -10,25 +10,29 @@
 #include <unistd.h>
 #include <time.h>
 
-#define NUMERO_VIATURAS 15
+#define MAXIMO_VIATURAS 20 // Número máximo de viaturas
+#define MAXIMO_OCORRENCIAS 50 // Número máximo de ocorrências
 
 // Variáveis que comandam o tempo da ocorrência
-const int MINIMO_DESLOCAMENTO = 2;
-const int MAXIMO_DESLOCAMENTO = 5;
-const int MINIMO_ATENDIMENTO = 8;
-const int MAXIMO_ATENDIMENTO = 20;
+int MINIMO_DESLOCAMENTO = 7;
+int MAXIMO_DESLOCAMENTO = 8;
+int MINIMO_ATENDIMENTO = 20;
+int MAXIMO_ATENDIMENTO = 15;
 
 // Variáveis relacionadas às viaturas
-pthread_t viaturas[NUMERO_VIATURAS]; // Cada thread é uma viatura
-int idsViaturas[NUMERO_VIATURAS]; // Array de identificador único de viaturas
+pthread_t viaturas[MAXIMO_VIATURAS]; // Cada thread é uma viatura
+int idsViaturas[MAXIMO_VIATURAS]; // Array de identificador único de viaturas
 
 // Variáveis relacionadas ao controle da central
-bool viaturasDisponiveis[NUMERO_VIATURAS] = {false}; // Array para verificar se a viatura está em uso
+bool viaturasDisponiveis[MAXIMO_VIATURAS] = {false}; // Array para verificar se a viatura está em uso
 int viaturasEmUso = 0;
+int ocorrencias_atendidas = 0;
+int ocorrencias_nao_atendidas = 0;
 pthread_mutex_t viaturasMutex = PTHREAD_MUTEX_INITIALIZER; // Mutex para fazer alterações nas variáveis acima de forma sincronizada
 
 // Variáveis relacionadas à dificuldade
-int chance_de_crime = 60;
+int chance_de_crime = 80;
+int viaturas_disponiveis = 4;
 
 // Gerar os identificadores únicos de cada viatura
 void inicializar_viaturas(int v[], int n) {
@@ -45,17 +49,18 @@ void* atender_ocorrencia(void* arg) {
     int deslocamento = rand() % MAXIMO_DESLOCAMENTO + MINIMO_DESLOCAMENTO;
     sleep(deslocamento); // Simula o tempo de deslocamento
 
-    printf("[VIATURA %04d] Chegou ao local da ocorrência em %d minutos\n", codigo, deslocamento);
+    printf("[VIATURA %04d] Chegou ao local da ocorrência em: %d minutos\n", codigo, deslocamento);
     sleep(rand() % MAXIMO_ATENDIMENTO + MINIMO_DESLOCAMENTO); // Simula o tempo de atendimento
 
     time_t finish = time(NULL); // Fim do atendimento
     long tempo = (long) difftime(finish, start); // Tempo do atendimento em 'minutos'
-    printf("[VIATURA %04d] Atendimento concluído em %lu minutos\n", codigo, tempo);
+    printf("[VIATURA %04d] Atendimento concluído em: %lu minutos\n", codigo, tempo);
 
-    pthread_mutex_lock(&viaturasMutex);
+    pthread_mutex_lock(&viaturasMutex); // Aciona o mutex para garantir acesso exclusivo
     viaturasDisponiveis[codigo - 1] = false; // Libera a viatura
     viaturasEmUso--; // Decrementa o contador de viaturas em uso
-    pthread_mutex_unlock(&viaturasMutex);
+    pthread_mutex_unlock(&viaturasMutex); // Libera o mutex
+
     pthread_exit(NULL); // Termina a thread
 }
 
@@ -63,30 +68,80 @@ int main() {
     // Inicializa um timer aleatório
     srand(time(NULL));
 
-    inicializar_viaturas(idsViaturas, NUMERO_VIATURAS);
+    // Criar os IDs únicos para cada viatura
+    inicializar_viaturas(idsViaturas, MAXIMO_VIATURAS);
 
     while (true) {
         int n = rand() % 101; // Gera um número de 0 a 100
+        
+        // Se o número de ocorrências atendidas e não atendidas for igual ao máximo de ocorrências, encerra o programa
+        if (ocorrencias_atendidas + ocorrencias_nao_atendidas == MAXIMO_OCORRENCIAS) {
+            printf("[CENTRAL] Todas as ocorrências foram atendidas...\n");
+            printf("[CENTRAL] Foram necessárias %d viaturas para atender as ocorrências\n", viaturas_disponiveis);
+            printf("[CENTRAL] Total de ocorrências atendidas: %d\n", ocorrencias_atendidas);
+            printf("[CENTRAL] Total de ocorrências não atendidas: %d\n", ocorrencias_nao_atendidas);
+            printf("[CENTRAL] Encerrando o programa...\n");
+            // Cancela todas as threads de viaturas
+            for (int i = 0; i < viaturas_disponiveis; i++) {
+                if (viaturasDisponiveis[i]) {
+                    pthread_cancel(viaturas[i]); // Cancela a thread da viatura
+                }
+            }
+            break; // Encerra o programa após 50 ocorrências
+        }
+
+        // A cada 20 ocorrências, aumentar a dificuldade
+        if ((ocorrencias_atendidas + ocorrencias_nao_atendidas) % 10 == 0 && ocorrencias_atendidas > 0) { // A cada 20 ocorrências atendidas, aumenta a dificuldade
+            if (chance_de_crime < 100) {
+                chance_de_crime += 10; // Aumenta a chance de crime
+            }
+            MINIMO_ATENDIMENTO+= 5; // Aumenta o tempo de atendimento
+            MINIMO_DESLOCAMENTO+= 3; // Aumenta o tempo de deslocamento
+            printf("[CENTRAL] Aumentando a dificuldade!\n");
+        }
 
         if (n < chance_de_crime) { // Se estiver abaixo da chance de crime, uma nova ocorrência é criada
             printf("[CENTRAL] Ocorrência detectada! Buscando viaturas...\n");
-            for (int i = 0; i < NUMERO_VIATURAS; i++) {
+            // Procura uma viatura disponível para atender a ocorrência
+            for (int i = 0; i < viaturas_disponiveis; i++) {
                 if (!viaturasDisponiveis[i]) { // Se a viatura não estiver em uso
                     pthread_mutex_lock(&viaturasMutex);
                     viaturasEmUso++; // Incrementa o número de viaturas em uso
                     viaturasDisponiveis[i] = true; // Marca a viatura como em uso
+                    ocorrencias_atendidas++; // Incrementa o número de ocorrências atendidas
                     pthread_mutex_unlock(&viaturasMutex);
+
+                    // Aciona a thread da viatura
                     pthread_create(&viaturas[i], NULL, atender_ocorrencia, &idsViaturas[i]);
-                    printf("[CENTRAL] Viatura %d designada para a ocorrência\n", i + 1);
+                    printf("[CENTRAL] Viatura %04d designada para a ocorrência!\n", i + 1);
                     break;
-                } else if (i == NUMERO_VIATURAS - 1) {
-                    printf("[CENTRAL] Todas as viaturas estão ocupadas\n");
+                }
+                // Se não foi possível encontrar uma viatura, incrementa o número de ocorrências não atendidas
+                else if (i == viaturas_disponiveis - 1) {
+                    ocorrencias_nao_atendidas++; // Incrementa o número de ocorrências não atendidas
+                    printf("[CENTRAL] Todas as viaturas estão ocupadas!\n");
                 }
             }
         } else {
             printf("[CENTRAL] Aguardando ocorrências...\n");
         }
-        printf("[CENTRAL] Total de viaturas em uso: %d\n\n", viaturasEmUso);
-        sleep(3); // Espera 3 minutos para verificar novas ocorrências
+        printf("[CENTRAL] Ocorrências atendidas: %d\n", ocorrencias_atendidas);
+        if (ocorrencias_nao_atendidas > 0) {
+            printf("[CENTRAL] Ocorrências não atendidas: %d\n", ocorrencias_nao_atendidas);
+        }
+
+        // Se o número de ocorrências não atendidas for maior que a metade do número de ocorrências atendidas
+        // e o número de viaturas disponíveis for menor que o máximo, aumenta o número de viaturas disponíveis
+        if (ocorrencias_nao_atendidas >= ocorrencias_atendidas / 2 && viaturas_disponiveis < MAXIMO_VIATURAS && ocorrencias_nao_atendidas > 0) {
+            viaturas_disponiveis += 2;
+            ocorrencias_nao_atendidas--; // Decrementa o número de ocorrências não atendidas
+            printf("[CENTRAL] Aumentando o número de viaturas disponíveis! A frota agora é de: %d viaturas\n", viaturas_disponiveis);
+        }
+
+        printf("[CENTRAL] Total de viaturas em uso: %d/%d\n\n", viaturasEmUso, viaturas_disponiveis);
+
+        sleep(2); // Espera para verificar novas ocorrências
     }
+
+    return EXIT_SUCCESS;
 }
